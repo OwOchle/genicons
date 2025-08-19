abaca1b082ddc11b6afcbf083b492a41f5a9eaaf
OwOchlé <romeoboub@gmail.com>
2025-08-19 12:40:44 +0200

    fix: fixed html link icons names

--- src/main.rs
+++ src/main.rs
@@ -134,8 +134,8 @@ fn main() -> ExitCode {
     }
 
     let html = r#"<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">
 <link rel="manifest" href="/manifest.json">"#;
 
     if !loop {
